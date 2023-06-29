051f5370ec8bfc003c36aa54e364201f7e20eaed
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-06-29 13:21:20 +0200

    Add files via upload

--- libraries/simphysiclab_ldr.py
+++ libraries/simphysiclab_ldr.py
@@ -350,6 +350,7 @@ def barridoCriterios(ax,TF,limites,paso,tolerancia):
   x = np.array(x)
   y = np.array(y)
   tolerancia=list(reversed(np.sort(tolerancia)))
+  print("tolerancia: ",tolerancia)
   dcrmnt=15
   for i in range(len(x)):
     maxDcrmnt=(len(tolerancia)+1)*dcrmnt+dcrmnt
