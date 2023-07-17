7cb5a680dcc18324fdb132533b9565d8add4207b
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-07-17 11:20:27 +0200

    Add files via upload

--- libraries/simphysiclab_pid.py
+++ libraries/simphysiclab_pid.py
@@ -513,7 +513,7 @@ def calculoParteProporcionalIntegradoraDiferencial(G,H,theta,wd,sgm,tipo_error,v
     if errValue==False:
       #PI controlador real e ideal
       zi=calculoZi(pto_interes)
-      pi=calculoPi(float(K)*PD,zi,tipo_error,valor_error)
+      pi=calculoPi(float(K)*G*PD,zi,tipo_error,valor_error)
       print(zi,pi)
       PI=SIS.generarTF("num_den",[1,-zi],[1-pi])
       return float(K),PD,PI
