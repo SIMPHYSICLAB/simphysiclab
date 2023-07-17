3ff64511625a1a0bf96a9df1550ff26ecf900161
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-07-17 10:25:20 +0200

    Add files via upload

--- libraries/simphysiclab_pid.py
+++ libraries/simphysiclab_pid.py
@@ -300,8 +300,7 @@ def comprobarLimitesConRestriccionesLDR(TF,theta=None,wd=None,sgm=None,maxK=1000
           return [x[firstElementComplex],y[firstElementComplex]],None
   """
   x,y=puntosEnAreaValidaSegunRestricciones(TF,theta,wd,sgm,maxK,paso)
-  print(x)
-  print(y)
+
   if x!=None:
     findElement=[element for element in y if element != 0][0]
     findLastElement=[element for element in reversed(y) if element != 0][0]
