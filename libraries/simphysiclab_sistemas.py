cf2286174887a36b71189b24d7da16dcfb984345
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-09-27 20:39:58 +0200

    Add files via upload

--- libraries/simphysiclab_sistemas.py
+++ libraries/simphysiclab_sistemas.py
@@ -1620,7 +1620,7 @@ def parametrosRespuestaTemporal(ax,valores,tiempo):
   a=max(y)-b
   errp=1-y[len(t)-1]
 
-  ax.set(xlim=(-5.5, t[len(t)-1]+1), ylim=(-1, max(y)+1))
+  ax.set(xlim=(-5.5, t[len(t)-1]+0.2), ylim=(-0.2, max(y)+0.2))
 
   ax.plot([0, 0], [0, y[len(t)-1]], c='green', ls='--', lw=1, alpha=1)
   ax.annotate('b=%s'%round(b,3),(0,b/2),(0,b/2))
