9cdc7594fa170f83bfa04afb4c5f100c5903306d
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-10-11 13:37:54 +0200

    Add files via upload

--- libraries/simphysiclab_ldr.py
+++ libraries/simphysiclab_ldr.py
@@ -339,19 +339,18 @@ def updateScatterLDRmanual(frame):
   #print(polos)
   for ptA in polos:
     if ptA.as_real_imag()[0]>xmins:
-
       points.append([ptA.as_real_imag()[0],ptA.as_real_imag()[1]])
 
-      plt.cla()
-      SIS.dibujarEjes(axs,1,[[xmins,xmaxs],[ymins,ymaxs]])
-      SIS.dibujarPolosCeros(axs,[[xmins,xmaxs],[ymins,ymaxs]],Gs*Hs)
-      axs.set_xlim(xmins, xmaxs)
-      axs.set_ylim(ymins, ymaxs)
-      for p in points:
-        axs.scatter(p[0],p[1],s=25,c='r', marker="o")
-      axs.annotate('K=%s'%round(frame,2),(0,-5),(0,-5))
-      for ptA2 in polos:
-        axs.annotate('ts=%s'%round(sympy.pi/abs(ptA2.as_real_imag()[0]),2),(ptA2.as_real_imag()[0],ptA2.as_real_imag()[1]),(ptA2.as_real_imag()[0],ptA2.as_real_imag()[1]))
+  plt.cla()
+  SIS.dibujarEjes(axs,1,[[xmins,xmaxs],[ymins,ymaxs]])
+  SIS.dibujarPolosCeros(axs,[[xmins,xmaxs],[ymins,ymaxs]],Gs*Hs)
+  axs.set_xlim(xmins, xmaxs)
+  axs.set_ylim(ymins, ymaxs)
+  for p in points:
+    axs.scatter(p[0],p[1],s=25,c='r', marker="o")
+  axs.annotate('K=%s'%round(frame,2),(0,-5),(0,-5))
+  for ptA2 in polos:
+    axs.annotate('ts=%s'%round(sympy.pi/abs(ptA2.as_real_imag()[0]),2),(ptA2.as_real_imag()[0],ptA2.as_real_imag()[1]),(ptA2.as_real_imag()[0],ptA2.as_real_imag()[1]))
 
 def barridoCriterios(ax,TF,limites,paso,tolerancia):
 
