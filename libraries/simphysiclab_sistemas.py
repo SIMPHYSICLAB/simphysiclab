644e3cfa9688d0664f88a1ef5bb3d78f4bb3f5fc
SIMPHYSICLAB <135227380+SIMPHYSICLAB@users.noreply.github.com>
2023-11-07 20:43:50 +0100

    Add files via upload

--- libraries/simphysiclab_sistemas.py
+++ libraries/simphysiclab_sistemas.py
@@ -1975,6 +1975,48 @@ def parametrosRespuestaTemporal(ax,y,t):
       ax.plot([tp, t[len(t)-1]], [y[len(t)-1], y[len(t)-1]], c='black', ls='--', lw=1, alpha=1)
     return (a/b)*100, tp,tr,errp
 
+def parametrosRespuestaTemporalSistema(TF):
+
+  tipo=SIS.tipoRespuestaNorden(TF)
+
+  y,t=SIS.respuestaEscalon(None,TF,20)
+
+  if tipo!=None and tipo !=-1:
+
+    b=y[len(t)-1]
+    a=max(y)-b
+    errp=1-y[len(t)-1]
+
+    print("Errp: ",errp)
+    print("valor final: ",b)
+
+    print("Sobreoscilacion: ",100*(a/b),"%")
+    print("b: ",b)
+    print("a: ",a)
+    print("Mp: ",(a/b)*100)
+    print("max: ",a+b)
+
+    tp=t[np.argmax(y)]
+    print("tp: ",tp,"s")
+
+    tr=[]
+    ytr=[]
+    vfind=y[len(t)-1]
+    for i in range(len(y)):
+      if vfind-y[i]<0:
+        tr=t[i-1]
+        ytr=y[i]
+        break
+    if ax!=None:
+      ax.annotate('tp=%s s'%round(tp,3),(tp,0.1),(tp,0.1))
+      ax.annotate('tr=%s s'%round(tr,3),(tr,-0.1),(tr,-0.1))
+      ax.plot([tr, tr], [0, ytr], c='green', ls='--', lw=1, alpha=1)
+      ax.plot([0, tp], [y[len(t)-1], y[len(t)-1]], c='red', ls='--', lw=1, alpha=1)
+      ax.plot([0, tp], [a+b, a+b], c='b', ls='--', lw=1, alpha=1)
+      ax.plot([tp, tp], [0, y[np.argmax(y)]], c='green', ls='--', lw=1, alpha=1)
+      ax.plot([tp, t[len(t)-1]], [y[len(t)-1], y[len(t)-1]], c='black', ls='--', lw=1, alpha=1)
+    return (a/b)*100, tp,tr,errp
+
 def parametrosTipoRegimen(ax,y,t):
 
 
